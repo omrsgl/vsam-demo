040b9d30b535deb6ce676cb2cca507bf076610ff
omer <omrsgl4@gmail.com>
2021-02-22 12:37:41 +0200

    Update projects

--- resources/scripts/tests/test-demo/test-demo-with-dynamic-redefines.sh
+++ resources/scripts/tests/test-demo/test-demo-with-dynamic-redefines.sh
@@ -5,7 +5,7 @@ echo Test VSAM Demo Started!
 
 ./olvsam.sh -record -add -lf ../cpy/F5750024.cpy -hdr ../cpy/F5750HIS.cpy -id 024C -redefines string
 ./olvsam.sh -record -add -lf ../cpy/F5750025.cpy -hdr ../cpy/F5750HIS.cpy -id 025C -redefines string
-./olvsam.sh -record -add -lf ../cpy/F5750040.cpy -hdr ../cpy/F5750HIS.cpy -id 040C -redefines string
+./olvsam.sh -record -add -lf ../cpy/F5750040.cpy -hdr ../cpy/F5750HIS.cpy -id 040C -redefines dynamic
 ./olvsam.sh -record -add -lf ../cpy/F5750044.cpy -hdr ../cpy/F5750HIS.cpy -id 044C -redefines string
 
 ./olvsam.sh -record -list
